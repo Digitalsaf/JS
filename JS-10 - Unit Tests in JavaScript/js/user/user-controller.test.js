db8b001632f6d0dff776950ff09e6f4511bb82d1
DigitalSaf <safwan.okomi@gmail.com>
2020-06-02 15:38:39 +0100

    fixed users

--- JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
+++ JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
@@ -16,3 +16,31 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+  test('add user to userController', () => {    
+    let user = new User(1235,"mmasd", "santiagtryto@generation.org");
+    expect(userController.getUsers()).not.toContain(user);
+    userController.add(user);  
+  });
+
+test('remove user to userController', () => {    
+    let user = new User(1251,"Sosfa", "santiagasdo@generation.org");   
+    expect(userController.users).not.toContain(user);
+    userController.remove(user);
+  });
+
+  test('find user by email ', () => {
+    let user= new User(2462, 'test', 'test1@mail.com');
+    userController.add(user);
+    userController.findByEmail('test1@mail.com');
+    expect(userController.findByEmail('test1@mail.com')).toEqual({"email": "test1@mail.com", "id": 2462, "name": "test"});
+  });
+  
+  test('find user by ID ', () => {
+    let user= new User(2470, 'test1', 'test12@mail.com');
+    userController.add(user);
+    userController.findById(2470);
+    expect(userController.findById(2470)).toEqual({"email": "test12@mail.com", "id": 2470, "name": "test1"});
+  });
+  
+
+
